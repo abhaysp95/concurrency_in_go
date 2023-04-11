4239e2a8d32ad7f3d197be0ba7690df487805023
abhay <abhaysp95@protonmail.com>
2023-04-11 18:02:01 +0530

    feat: using RWMutex (to have more control)

--- building_blocks/main.go
+++ building_blocks/main.go
@@ -1,5 +1,5 @@
 package building_blocks
 
 func Main() {
-	justMutexIt()
+	omaewaRWMutex()
 }
--- building_blocks/using_mutex.go
+++ building_blocks/using_mutex.go
@@ -2,7 +2,11 @@ package building_blocks
 
 import (
 	"fmt"
+	"math"
+	"os"
 	"sync"
+	"text/tabwriter"
+	"time"
 )
 
 // handling concurrency through memory access synchronization
@@ -45,4 +49,45 @@ func justMutexIt() {
 	}
 
 	wg.Wait()
+}
+
+func omaewaRWMutex() {
+	producer := func(wg *sync.WaitGroup, l sync.Locker) {
+		defer wg.Done()
+		for i := 0; i < 5; i++ {
+			l.Lock()
+			l.Unlock()
+			time.Sleep(1)
+		}
+	}
+
+	consumer := func(wg *sync.WaitGroup, l sync.Locker) {
+		defer wg.Done()
+		l.Lock()
+		l.Unlock()
+	}
+
+	test := func(count int, mutex, rwMutex sync.Locker) time.Duration {
+		var wg sync.WaitGroup
+		wg.Add(count + 1)
+		begin := time.Now()
+		go producer(&wg, mutex)
+		for i := 0; i < count; i++ {
+			go consumer(&wg, rwMutex)
+		}
+
+		wg.Wait()
+		return time.Since(begin)
+	}
+
+	tw := tabwriter.NewWriter(os.Stdout, 0, 1, 2, ' ', 0)
+	defer tw.Flush()
+
+	var m sync.RWMutex
+	fmt.Fprintf(tw, "Readers\tRWMutex\tMutex\n")
+
+	for i := 0; i < 20; i++ {
+		count := int(math.Pow(2, float64(i)))
+		fmt.Fprintf(tw, "%d\t%v\t%v\n", count, test(count, &m, m.RLocker()), test(count, &m, &m))
+	}
 }
